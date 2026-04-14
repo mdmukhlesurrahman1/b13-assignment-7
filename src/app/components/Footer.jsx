b2b5f6f48477a025aa200e0aaa7dbbd88910c1d9
mdmukhlesurrahman1 <mdmukhlesurrahman1@gmail.com>
2026-04-14 17:10:08 +0600

    footer design and initial satup

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,46 @@
+import {
+  FaSquareFacebook,
+  FaSquareInstagram,
+  FaXTwitter,
+} from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#244D3F] text-white pt-10 md:pt-20 pb-7.5">
+      <div className="container mx-auto px-4 md:px-20">
+        <div className="flex flex-col justify-center items-center text-center">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4">KeenKeeper</h1>
+          <p className="mb-6">
+            Your personal shelf of meaningful connections. Browse, tend, and
+            nurture the relationships that matter most.
+          </p>
+          <h4 className="mb-4 text-xl md:text-2xl font-semibold">
+            Social Links
+          </h4>
+          <div className="flex gap-3 mb-10">
+            <div className="w-10 h-10 bg-white rounded-full text-black flex justify-center items-center text-xl">
+              <FaSquareInstagram />
+            </div>
+            <div className="w-10 h-10 bg-white rounded-full text-black flex justify-center items-center text-xl">
+              <FaSquareFacebook />
+            </div>
+            <div className="w-10 h-10 bg-white rounded-full text-black flex justify-center items-center text-xl">
+              <FaXTwitter />
+            </div>
+          </div>
+          <hr className="mb-7.5 w-full opacity-50" />
+          <div className="flex flex-col md:flex-row w-full items-center md:justify-between gap-4">
+            <p>© 2026 KeenKeeper. All rights reserved.</p>
+            <div className="flex gap-4">
+            <p>Privacy Policy</p>
+            <p>Terms of Service</p>
+            <p>Cookies</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="container mx-auto px-4">
+            This is Header.............
+        </div>
+    );
+};
+
+export default Header;
